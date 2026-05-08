852f51e75c6a9f7ff055037a3d8f0bb295000f36
REIDNER FAUSTO MARQUES <reidner.marques@univille.br>
2026-05-08 19:58:52 -0300

    b

--- src/function_app.py
+++ src/function_app.py
@@ -46,12 +46,7 @@ def extract_csat_avaliacao(myTimer: func.TimerRequest) -> None:
 @app.timer_trigger(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
 def extract_fila(myTimer: func.TimerRequest) -> None:
-    logging.info('tabela fila')    
-
-@app.timer_trigger(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=False,
-              use_monitor=False) 
-def extract_sla(myTimer: func.TimerRequest) -> None:
-    logging.info('tabela sla')    
+    logging.info('tabela fila')        
 
 @app.timer_trigger(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
