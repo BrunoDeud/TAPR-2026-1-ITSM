ba748e06342520b5356f2f7190b1096d3dc055a5
REIDNER FAUSTO MARQUES <reidner.marques@univille.br>
2026-05-08 19:48:37 -0300

    a

--- src/function_app.py
+++ src/function_app.py
@@ -15,30 +15,45 @@ def extract_sla(myTimer: func.TimerRequest) -> None:
 
 @app.timer_trigger(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
-def extract_sla(myTimer: func.TimerRequest) -> None:
+def extract_chamado_sla(myTimer: func.TimerRequest) -> None:
     logging.info('tabela chamado_sla')    
  
 @app.timer_trigger(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
-def extract_sla(myTimer: func.TimerRequest) -> None:
+def extract_analista(myTimer: func.TimerRequest) -> None:
     logging.info('tabela analista')    
    
 @app.timer_trigger(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
-def extract_sla(myTimer: func.TimerRequest) -> None:
+def extract_categoria(myTimer: func.TimerRequest) -> None:
     logging.info('tabela categoria')    
  
 @app.timer_trigger(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
-def extract_sla(myTimer: func.TimerRequest) -> None:
+def extract_chamada_status_historico(myTimer: func.TimerRequest) -> None:
     logging.info('tabela chamada_status_historico')    
   
 @app.timer_trigger(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
-def extract_sla(myTimer: func.TimerRequest) -> None:
+def extract_cliente_organizacao(myTimer: func.TimerRequest) -> None:
     logging.info('tabela cliente_organizacao')    
  
 @app.timer_trigger(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
-def extract_sla(myTimer: func.TimerRequest) -> None:
+def extract_csat_avaliacao(myTimer: func.TimerRequest) -> None:
     logging.info('tabela csat_avaliacao')    
+
+@app.timer_trigger(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def extract_fila(myTimer: func.TimerRequest) -> None:
+    logging.info('tabela fila')    
+
+@app.timer_trigger(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def extract_sla(myTimer: func.TimerRequest) -> None:
+    logging.info('tabela sla')    
+
+@app.timer_trigger(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def extract_solicitante(myTimer: func.TimerRequest) -> None:
+    logging.info('tabela solicitante')    
