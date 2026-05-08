29b6185dff0dc6c559f1e234878ab24e5ba45423
REIDNER FAUSTO MARQUES <reidner.marques@univille.br>
2026-05-08 20:30:46 -0300

    d

--- src/function_app.py
+++ src/function_app.py
@@ -3,15 +3,16 @@ import azure.functions as func
 
 app = func.FunctionApp()            
 
-from triggers.extract_solicitante import app as solicitante
 from triggers.extract_analista import app as analista
 from triggers.extract_categoria import app as categoria
 from triggers.extract_chamada_status_historico import app as hamada_status_historico
 from triggers.extract_chamado_sla import app as chamado_sla
-from triggers.extract_cliente_organizacao import app as cextract_cliente_organizacao
-from triggers.extract_csat_avaliacao import app as sat_avaliacao
+from triggers.extract_chamado import app as chamado
+from triggers.extract_cliente_organizacao import app as cliente_organizacao
+from triggers.extract_csat_avaliacao import app as csat_avaliacao
 from triggers.extract_fila import app as fila
 from triggers.extract_sla import app as sla
+from triggers.extract_solicitante import app as solicitante
 
 
 app.register_function(extract_chamado)
