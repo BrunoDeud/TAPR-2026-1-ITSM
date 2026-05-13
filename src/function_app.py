ec4f569979d43e5784ea4cf4a8960d2e129667b1
BRUNO DEUD <bruno.deud@univille.br>
2026-05-12 21:59:43 -0300

    Blueprint 2

--- src/function_app.py
+++ src/function_app.py
@@ -1,28 +1,26 @@
 import logging
 import azure.functions as func
 
-app = func.FunctionApp()            
+app = func.FunctionApp()
 
-from triggers.extract_analista import app as analista
-from triggers.extract_categoria import app as categoria
-from triggers.extract_chamada_status_historico import app as chamada_status_historico
-from triggers.extract_chamado_sla import app as chamado_sla
-from triggers.extract_chamado import app as chamado
-from triggers.extract_cliente_organizacao import app as cliente_organizacao
-from triggers.extract_csat_avaliacao import app as csat_avaliacao
-from triggers.extract_fila import app as fila
-from triggers.extract_sla import app as sla
-from triggers.extract_solicitante import app as solicitante
+from triggers.extract_analista import app as extract_analista
+from triggers.extract_categoria import app as extract_categoria
+from triggers.extract_chamado_sla import app as extract_chamado_sla
+from triggers.extract_chamada_status_historico import app as extract_chamada_status_historico
+from triggers.extract_chamado import app as extract_chamado
+from triggers.extract_cliente_organizacao import app as extract_cliente_organizacao
+from triggers.extract_csat_avaliacao import app as extract_csat_avaliacao
+from triggers.extract_fila import app as extract_fila
+from triggers.extract_sla import app as extract_sla
+from triggers.extract_solicitante import app as extract_solicitante
 
-
-app.register_function(chamado)
-app.register_function(analista)
-app.register_function(solicitante)
-app.register_function(categoria)
-app.register_function(chamado_sla)
-app.register_function(cliente_organizacao)
-app.register_function(csat_avaliacao)
-app.register_function(fila)
-app.register_function(sla)
-app.register_function(chamada_status_historico)
-
+app.register_functions(extract_analista)
+app.register_functions(extract_categoria)
+app.register_functions(extract_chamado_sla)
+app.register_functions(extract_chamada_status_historico)
+app.register_functions(extract_chamado)
+app.register_functions(extract_cliente_organizacao)
+app.register_functions(extract_csat_avaliacao)
+app.register_functions(extract_fila)
+app.register_functions(extract_sla)
+app.register_functions(extract_solicitante)
