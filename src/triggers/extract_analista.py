66e0f3af9b7ed714277c6e2ecdfd513b8b34087e
REIDNER FAUSTO MARQUES <reidner.marques@univille.br>
2026-05-22 19:34:48 -0300

    completa o codigo

--- src/triggers/extract_analista.py
+++ src/triggers/extract_analista.py
@@ -18,3 +18,36 @@ def extract_analista(myTimer: func.TimerRequest) -> None:
     sql_pass = os.getenv("SQL_PASSWORD_SOURCE")
 
     logging.info(f'Servidor: {sql_server}, Banco: {sql_database} , User: {sql_user}, Senha: {sql_pass}')
+
+        # Configura a string de conexão para o banco de dados SQL Server
+    conn_str = (
+        "DRIVER={ODBC Driver 18 for SQL Server};"
+        f"SERVER={sql_server};"
+        f"DATABASE={sql_database};"
+        f"UID={sql_user};"
+        f"PWD={sql_pass};"
+        "Encrypt=yes;"
+        "TrustServerCertificate=no;"
+        "Connection Timeout=30;"
+    )
+
+   
+    try:
+        # Estabelece a conexão com o banco de dados usando pyodbc
+        with pyodbc.connect(conn_str) as conn:
+            # Cria um cursor para executar a consulta   
+            cursor = conn.cursor()
+            
+            query = "select top 10 * from itsm.analista"
+
+            # Executa a consulta SQL
+            cursor.execute(query)
+
+            # Busca todos os resultados da consulta
+            rows = cursor.fetchall()
+
+            logging.info(rows)           
+
+    except Exception as e:
+        logging.error(f"Erro ao ler itsm.analista: {str(e)}")
+        raise
--- src/triggers/extract_chamada_status_historico.py
+++ src/triggers/extract_chamada_status_historico.py
@@ -1,6 +1,8 @@
 import logging
 import azure.functions as func
 import os
+import pyodbc
+
 app = func.Blueprint()
  
 @app.timer_trigger(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=False,
@@ -14,3 +16,36 @@ def extract_chamada_status_historico(myTimer: func.TimerRequest) -> None:
     sql_pass = os.getenv("SQL_PASSWORD_SOURCE")
 
     logging.info(f'Servidor: {sql_server}, Banco: {sql_database} , User: {sql_user}, Senha: {sql_pass}')
+
+        # Configura a string de conexão para o banco de dados SQL Server
+    conn_str = (
+        "DRIVER={ODBC Driver 18 for SQL Server};"
+        f"SERVER={sql_server};"
+        f"DATABASE={sql_database};"
+        f"UID={sql_user};"
+        f"PWD={sql_pass};"
+        "Encrypt=yes;"
+        "TrustServerCertificate=no;"
+        "Connection Timeout=30;"
+    )
+
+   
+    try:
+        # Estabelece a conexão com o banco de dados usando pyodbc
+        with pyodbc.connect(conn_str) as conn:
+            # Cria um cursor para executar a consulta   
+            cursor = conn.cursor()
+            
+            query = "select top 10 * from itsm.chamada_status_historico"
+
+            # Executa a consulta SQL
+            cursor.execute(query)
+
+            # Busca todos os resultados da consulta
+            rows = cursor.fetchall()
+
+            logging.info(rows)           
+
+    except Exception as e:
+        logging.error(f"Erro ao ler itsm.chamada_status_historico: {str(e)}")
+        raise
